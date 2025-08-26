95f5c746c643a4ace35d72b0c4879e978cb915d6
pradoscript <prado.joao06480@gmail.com>
2025-08-25 21:40:27 -0300

    feat: initialize server

---
+++ backend/src/app.ts
@@ -0,0 +1,13 @@
+import express from "express"
+import { routes } from "@/routes"
+import { errorHandling } from "./middlewares/errorHandling"
+import cors from "cors"
+
+const app = express()
+app.use(cors({
+    origin: "http://localhost:5173"
+}))
+app.use(express.json())
+app.use(routes)
+app.use(errorHandling)
+export { app }
---
+++ backend/src/server.ts
@@ -0,0 +1,8 @@
+import { app } from "@/app"
+import 'dotenv/config'
+
+
+const PORT = 3333
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
